ac600b66a1fd0c0f1439a783e2ce015f571d3630
VictorNorman <vtn2@calvin.edu>
2024-05-14 11:40:21 -0400

    add functions.* and remove main.cpp

---
+++ functions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void vars() {
+    // Add Step 3 code below this line.
+
+    // Add Step 4 code below this line.
+}
+
+// Step 5
+void ns() {
+}
+
+// Step 6
+void arrays() {
+}
+
+// Step 7
+void dec() {
+}
---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+void vars();
+void ns();
+void arrays();
+void dec();
+
+#endif /* FUNCTIONS_H_ */
