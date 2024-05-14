876cd7829f94017f4e9298bdc2eebd7ddefce326
VictorNorman <vtn2@calvin.edu>
2024-05-14 11:35:11 -0400

    add catch.hpp and tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "functions.h"
+
+TEST_CASE("step5") {
+    ns();
+}
+
+TEST_CASE("step6") {
+    arrays();
+}
+
+TEST_CASE("step7") {
+    dec();
+}
